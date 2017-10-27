1410e7de67f0382ba5c62edb763c05ea9693c70d
Maria Burlando <maria@dmm888.com>
2017-10-27 17:04:32 +0100

    Setting up Gulp tasks

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+﻿var gulp = require('gulp');
+// Requires the gulp-sass plugin
+var sass = require('gulp-sass');
+var webserver = require('gulp-webserver');
+
+gulp.task('sass', function () {
+    return gulp.src('scss/**/*.scss')
+        .pipe(sass()) // Using gulp-sass
+        .pipe(gulp.dest('css'));
+});
+
+gulp.task('watch', function () {
+    gulp.watch('scss/**/*.scss', ['sass']);   
+});
+ 
+gulp.task('webserver', function() {
+  gulp.src('./')
+    .pipe(webserver({
+      livereload: true,
+      directoryListing: false,
+      open: true
+    }));
+});
+
+
+gulp.task('default', ['sass','webserver','watch']);
+
