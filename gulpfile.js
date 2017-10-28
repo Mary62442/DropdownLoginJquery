5fc287e62108019fa8caefa1e6a71e2b10837c8d
David <david@dmm888.com>
2017-10-28 15:14:24 +0200

    Implemented minification and uglify tasks without using run-sequence

--- gulpfile.js
+++ gulpfile.js
@@ -5,24 +5,12 @@ var webserver = require('gulp-webserver');
 var uglify = require('gulp-uglify');
 var uglifycss = require('gulp-uglifycss');
 var pump = require('pump');
-var runSequence = require('run-sequence');
 
+//In order for tasks to work in sequence
+//make sure the tasks return a stream or promise, or handle the callback
 
-gulp.task('buildcss', function (cb){
-runSequence ( 'sass', 'uglifycss', cb);
-});
-
-//In order for run-sequence to work properly
-//make sure they either return a stream or promise, or handle the callback
-
-gulp.task('sass', function () {
-   return  gulp.src('scss/**/*.scss')
-        .pipe(sass()) 
-        .pipe(gulp.dest('css'));
-});
-
-gulp.task('uglifycss', function () {
-return  gulp.src('css/**/*.css')
+gulp.task('uglifycss', ['sass'], function() {
+  return  gulp.src('css/**/*.css')
     .pipe(uglifycss({
       "maxLineLen": 80,
       "uglyComments": true
@@ -30,9 +18,16 @@ return  gulp.src('css/**/*.css')
     .pipe(gulp.dest('dist'));
 });
 
+// The task that depends on uglifycss task and must be executed first
+gulp.task('sass', function() {
+      return   gulp.src('scss/**/*.scss')
+        .pipe(sass()) 
+        .pipe(gulp.dest('css'));   
+});
+
 
 gulp.task('watch', function () {
-    gulp.watch('scss/**/*.scss', ['buildcss']);
+    gulp.watch('scss/**/*.scss', ['uglifycss']);
     gulp.watch('scripts/**/*.js', ['compressjs']);
 });
  
@@ -56,5 +51,5 @@ gulp.task('compressjs', function (cb) {
 });
 
 
-gulp.task('default', ['buildcss','webserver','watch', 'compressjs']);
+gulp.task('default', ['uglifycss','webserver','watch', 'compressjs']);
 
